bdc75b8e3f21dcd7dd82a5566b16631c86de7a95
maluz <marco.luzzara@hotmail.it>
2019-08-31 00:59:23 +0200

    added component load when questionnaire is open

--- QuestionLearner/Forms/MainForm.Designer.cs
+++ QuestionLearner/Forms/MainForm.Designer.cs
@@ -36,7 +36,17 @@
             this.toolsMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.createQuestionnaireMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.dlgOpenQuestionnaire = new System.Windows.Forms.OpenFileDialog();
+            this.lblQuestionText = new System.Windows.Forms.Label();
+            this.pnlQuestion = new System.Windows.Forms.Panel();
+            this.gBoxAnswers = new System.Windows.Forms.GroupBox();
+            this.radioBtnFalse = new System.Windows.Forms.RadioButton();
+            this.radioBtnTrue = new System.Windows.Forms.RadioButton();
+            this.pBoxImgDisplay = new System.Windows.Forms.PictureBox();
+            this.lblCorrectAnswer = new System.Windows.Forms.Label();
             this.mainMenuStrip.SuspendLayout();
+            this.pnlQuestion.SuspendLayout();
+            this.gBoxAnswers.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxImgDisplay)).BeginInit();
             this.SuspendLayout();
             // 
             // mainMenuStrip
@@ -46,7 +56,7 @@
             this.toolsMenuItem});
             this.mainMenuStrip.Location = new System.Drawing.Point(0, 0);
             this.mainMenuStrip.Name = "mainMenuStrip";
-            this.mainMenuStrip.Size = new System.Drawing.Size(1037, 24);
+            this.mainMenuStrip.Size = new System.Drawing.Size(744, 24);
             this.mainMenuStrip.TabIndex = 0;
             this.mainMenuStrip.Text = "mainMenuStrip";
             // 
@@ -63,20 +73,20 @@
             // openMenuItem
             // 
             this.openMenuItem.Name = "openMenuItem";
-            this.openMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openMenuItem.Size = new System.Drawing.Size(110, 22);
             this.openMenuItem.Text = "Open";
             this.openMenuItem.Click += new System.EventHandler(this.openMenuItem_Click);
             // 
             // recentMenuItem
             // 
             this.recentMenuItem.Name = "recentMenuItem";
-            this.recentMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.recentMenuItem.Size = new System.Drawing.Size(110, 22);
             this.recentMenuItem.Text = "Recent";
             // 
             // closeMenuItem
             // 
             this.closeMenuItem.Name = "closeMenuItem";
-            this.closeMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.closeMenuItem.Size = new System.Drawing.Size(110, 22);
             this.closeMenuItem.Text = "Close";
             this.closeMenuItem.Click += new System.EventHandler(this.closeMenuItem_Click);
             // 
@@ -95,11 +105,94 @@
             this.createQuestionnaireMenuItem.Text = "Create Questionnaire";
             this.createQuestionnaireMenuItem.Click += new System.EventHandler(this.createQuestionnaireMenuItem_Click);
             // 
+            // lblQuestionText
+            // 
+            this.lblQuestionText.AutoSize = true;
+            this.lblQuestionText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestionText.Location = new System.Drawing.Point(3, 3);
+            this.lblQuestionText.Name = "lblQuestionText";
+            this.lblQuestionText.Size = new System.Drawing.Size(75, 17);
+            this.lblQuestionText.TabIndex = 1;
+            this.lblQuestionText.Text = "Test Label";
+            this.lblQuestionText.Visible = false;
+            // 
+            // pnlQuestion
+            // 
+            this.pnlQuestion.AutoScroll = true;
+            this.pnlQuestion.Controls.Add(this.lblQuestionText);
+            this.pnlQuestion.Location = new System.Drawing.Point(21, 41);
+            this.pnlQuestion.Name = "pnlQuestion";
+            this.pnlQuestion.Size = new System.Drawing.Size(437, 155);
+            this.pnlQuestion.TabIndex = 2;
+            this.pnlQuestion.Visible = false;
+            // 
+            // gBoxAnswers
+            // 
+            this.gBoxAnswers.Controls.Add(this.radioBtnFalse);
+            this.gBoxAnswers.Controls.Add(this.radioBtnTrue);
+            this.gBoxAnswers.Location = new System.Drawing.Point(522, 44);
+            this.gBoxAnswers.Name = "gBoxAnswers";
+            this.gBoxAnswers.Size = new System.Drawing.Size(200, 80);
+            this.gBoxAnswers.TabIndex = 3;
+            this.gBoxAnswers.TabStop = false;
+            this.gBoxAnswers.Text = "Answer";
+            this.gBoxAnswers.Visible = false;
+            // 
+            // radioBtnFalse
+            // 
+            this.radioBtnFalse.AutoSize = true;
+            this.radioBtnFalse.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioBtnFalse.Location = new System.Drawing.Point(20, 46);
+            this.radioBtnFalse.Name = "radioBtnFalse";
+            this.radioBtnFalse.Size = new System.Drawing.Size(60, 21);
+            this.radioBtnFalse.TabIndex = 1;
+            this.radioBtnFalse.TabStop = true;
+            this.radioBtnFalse.Text = "False";
+            this.radioBtnFalse.UseVisualStyleBackColor = true;
+            this.radioBtnFalse.Visible = false;
+            // 
+            // radioBtnTrue
+            // 
+            this.radioBtnTrue.AutoSize = true;
+            this.radioBtnTrue.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioBtnTrue.Location = new System.Drawing.Point(20, 19);
+            this.radioBtnTrue.Name = "radioBtnTrue";
+            this.radioBtnTrue.Size = new System.Drawing.Size(56, 21);
+            this.radioBtnTrue.TabIndex = 0;
+            this.radioBtnTrue.TabStop = true;
+            this.radioBtnTrue.Text = "True";
+            this.radioBtnTrue.UseVisualStyleBackColor = true;
+            this.radioBtnTrue.Visible = false;
+            // 
+            // pBoxImgDisplay
+            // 
+            this.pBoxImgDisplay.Location = new System.Drawing.Point(21, 213);
+            this.pBoxImgDisplay.Name = "pBoxImgDisplay";
+            this.pBoxImgDisplay.Size = new System.Drawing.Size(701, 384);
+            this.pBoxImgDisplay.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pBoxImgDisplay.TabIndex = 4;
+            this.pBoxImgDisplay.TabStop = false;
+            this.pBoxImgDisplay.Visible = false;
+            // 
+            // lblCorrectAnswer
+            // 
+            this.lblCorrectAnswer.AutoSize = true;
+            this.lblCorrectAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCorrectAnswer.Location = new System.Drawing.Point(523, 148);
+            this.lblCorrectAnswer.Name = "lblCorrectAnswer";
+            this.lblCorrectAnswer.Size = new System.Drawing.Size(0, 17);
+            this.lblCorrectAnswer.TabIndex = 2;
+            this.lblCorrectAnswer.Visible = false;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1037, 637);
+            this.ClientSize = new System.Drawing.Size(744, 617);
+            this.Controls.Add(this.lblCorrectAnswer);
+            this.Controls.Add(this.pBoxImgDisplay);
+            this.Controls.Add(this.gBoxAnswers);
+            this.Controls.Add(this.pnlQuestion);
             this.Controls.Add(this.mainMenuStrip);
             this.MainMenuStrip = this.mainMenuStrip;
             this.Name = "MainForm";
@@ -107,6 +200,11 @@
             this.Load += new System.EventHandler(this.mainForm_Load);
             this.mainMenuStrip.ResumeLayout(false);
             this.mainMenuStrip.PerformLayout();
+            this.pnlQuestion.ResumeLayout(false);
+            this.pnlQuestion.PerformLayout();
+            this.gBoxAnswers.ResumeLayout(false);
+            this.gBoxAnswers.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxImgDisplay)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,6 +220,13 @@
         private System.Windows.Forms.ToolStripMenuItem toolsMenuItem;
         private System.Windows.Forms.ToolStripMenuItem createQuestionnaireMenuItem;
         private System.Windows.Forms.OpenFileDialog dlgOpenQuestionnaire;
+        private System.Windows.Forms.Label lblQuestionText;
+        private System.Windows.Forms.Panel pnlQuestion;
+        private System.Windows.Forms.GroupBox gBoxAnswers;
+        private System.Windows.Forms.RadioButton radioBtnFalse;
+        private System.Windows.Forms.RadioButton radioBtnTrue;
+        private System.Windows.Forms.PictureBox pBoxImgDisplay;
+        private System.Windows.Forms.Label lblCorrectAnswer;
     }
 }
 
--- QuestionLearner/Forms/MainForm.cs
+++ QuestionLearner/Forms/MainForm.cs
@@ -16,6 +16,9 @@ namespace QuestionLearner
 {
     public partial class MainForm : Form
     {
+        protected Queue<Question> questions;
+        protected IDictionary<string, string> resources;
+
         public MainForm()
         {
             InitializeComponent();
@@ -37,6 +40,41 @@ namespace QuestionLearner
             form.ShowDialog();
         }
 
+        protected virtual IEnumerable<Question> GetNextQuestion()
+        {
+            if (questions.Count > 0)
+            {
+                var question = questions.Dequeue();
+                questions.Enqueue(question);
+
+                yield return question;
+            }
+            else
+            {
+                yield return null;
+            }
+        }
+
+        protected virtual void LoadQuestionOnForm()
+        {
+            var curQuestion = GetNextQuestion().First();
+
+            this.lblQuestionText.Text = curQuestion.Text;
+            this.pBoxImgDisplay.Image = curQuestion.Resource?.Image;
+        }
+
+        protected virtual void InitComponentForQuestionnaire()
+        {
+            this.lblQuestionText.Visible = true;
+            this.pnlQuestion.Visible = true;
+            this.pBoxImgDisplay.Visible = true;
+            this.gBoxAnswers.Visible = true;
+            this.radioBtnTrue.Visible = true;
+            this.radioBtnFalse.Visible = true;
+
+            LoadQuestionOnForm();
+        }
+
         private void openMenuItem_Click(object sender, EventArgs e)
         {
             if (this.dlgOpenQuestionnaire.ShowDialog() == DialogResult.OK)
@@ -47,11 +85,14 @@ namespace QuestionLearner
                 var questionsElems = questionnaireXml.XPathSelectElements("questions/question");
                 var resourcesElems = questionnaireXml.XPathSelectElements("resources/resource");
 
-                IDictionary<string, string> resources = resourcesElems.ToDictionary(
+                resources = resourcesElems.ToDictionary(
                     elem => elem.Attribute("id").Value,
                     elem => elem.Value
                 );
-                IEnumerable<Question> questions = questionsElems.Select(elem => QuestionExtension.FromXml(elem, resources));
+                questions = new Queue<Question>(questionsElems.Select(elem => QuestionExtension.FromXml(elem, resources)));
+
+                if (questions.Count > 0)
+                    InitComponentForQuestionnaire();
             }
         }
     }
